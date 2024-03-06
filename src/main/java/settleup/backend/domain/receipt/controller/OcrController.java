806c546a8070fa6cb05278abbe674a5f7e21621f
donghee9 <129722492+donghee9@users.noreply.github.com>
2024-03-06 13:10:53 +0900

    merge 1.kakaoLogin-ref 2.email-search-ref 3.create-group 4.ocr-bridge 5.create-transaction-receipt

--- src/main/java/settleup/backend/domain/receipt/controller/OcrController.java
+++ src/main/java/settleup/backend/domain/receipt/controller/OcrController.java
@@ -12,11 +12,11 @@ import settleup.backend.domain.receipt.receiptCommons.ControllerHelper;
 import settleup.backend.domain.receipt.service.OcrService;
 
 import java.io.IOException;
+
 import org.springframework.web.context.request.async.DeferredResult;
 import settleup.backend.global.api.ResponseDto;
 import settleup.backend.domain.user.service.LoginService;
 import settleup.backend.global.exception.CustomException;
-
 
 
 @RestController
@@ -66,7 +66,7 @@ public class OcrController {
 
     @PostMapping("/create")
     public ResponseDto createReceipt(
-            @RequestHeader(value = "Authorization") String token,@RequestBody ReceiptRequestDto requestDto) throws CustomException{
+            @RequestHeader(value = "Authorization") String token, @RequestBody ReceiptRequestDto requestDto) throws CustomException {
         loginService.validTokenOrNot(token);
         String missingFields = ControllerHelper.checkRequiredWithFilter(requestDto);
         if (!missingFields.isEmpty()) {
@@ -74,7 +74,7 @@ public class OcrController {
             return errorResponse;
         }
         ResponseDto successResponse =
-                new ResponseDto<>(true,"수빈님 요청은 현재 1.토큰인증성공 2.필드값 누락이 없는 상태입니다 (유저 , 그룹 검증은 미확인) 백엔드에서 잘 받았습니다. 백 로직이 현재 유저 검증, 그룹검증, 비용로직이 진행 중이므로  성공시 정확한 body값은 추후에 드리겠습니다",null);
+                new ResponseDto<>(true, "수빈님 요청은 현재 1.토큰인증성공 2.필드값 누락이 없는 상태입니다 (유저 , 그룹 검증은 미확인) 백엔드에서 잘 받았습니다. 백 로직이 현재 유저 검증, 그룹검증, 비용로직이 진행 중이므로  성공시 정확한 body값은 추후에 드리겠습니다", null);
         return successResponse;
     }
 }
